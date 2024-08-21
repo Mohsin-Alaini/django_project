cdb03cfa3ea55e5dea8050af78278293f666cdef
Mohsin-Alaini <mohsin.alaini2020@gmail.com>
2024-08-21 20:37:48 +0300

    start in the views

--- core/apps/accounts/views.py
+++ core/apps/accounts/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from ..models import Currency,ExchangeRate
+from django.db.models import Q , Max , Min , Avg ,Sum, F
+from ..models import Currency,ExchangeRate,Account,TransactionDetails,Transaction
 # Create your views here.
 def view(request):
     # Currency.objects.create(code = 'YER',name = "Yemeni",local= True)
     # cur = Currency.objects.all().first()
     # ExchangeRate.objects.create(currency=cur, exchange_rate=1,max_exchange_rate=1,min_exchange_rate=1)
     # print(cur,'pppppppppppp')    
-    exchangeRate = ExchangeRate.objects.values('exchange_rate','currency__name')
-    currency = Currency.objects.values('id','name','exchangerate__exchange_rate')
+    # exchangeRate = ExchangeRate.objects.values('exchange_rate','currency__name')
+    # currency = Currency.objects.values('id','name','exchangerate__exchange_rate')
+    clint_trans = Account.objects.filter(name='client2').values('name','transactiondetails__debit','transactiondetails__credit')     
+    transactions = TransactionDetails.objects.filter(~Q(account__number='1',transaction__date__gte='2024-05-10',account__name='client1')).aggregate(sum_debit=Sum('debit'),sum_credit=Sum('credit'))
+    transactions = TransactionDetails.objects.values('account__name','currency__name').annotate(sum_debit=Sum('debit'),sum_credit=Sum('credit')).annotate(balance=F('sum_debit')-F('sum_credit'))
     
-    for cu in currency:
+    print('----------')
+    print(transactions)
+    print('----------') 
+    for cu in transactions:
         print(cu)    
     return HttpResponse('view1')
 
